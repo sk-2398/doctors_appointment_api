a2be2e45a70a84240c763f8121345971dbc57b1c
sk-2398 <suyogkapdekar.23@gmail.com>
2024-04-20 15:07:50 +0530

    requirements added

--- doctor_project/settings.py
+++ doctor_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#l#p54q(x2l&v6*cy)wk)meq62dj@#c$!^ge9d*s-^ek3)0q%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -74,14 +74,15 @@ WSGI_APPLICATION = 'doctor_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES=dj_database_url.parse('postgres://root:A1ADF4LbY2vt2QLvpSkqmcntYqJPc8pN@dpg-cohola779t8c7385e3o0-a/appointmentdb_5egk')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
